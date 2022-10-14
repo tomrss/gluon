67259538ea93588675a2f6ba131919ad2acfd34c
Tommaso Rossi <tommaso.rossi1@protonmail.com>
2022-10-15 00:18:40 +0200

    Maven plugin

--- gluon-maven-plugin/src/main/java/io/tomrss/gluon/mavenplugin/CreateProjectMojo.java
+++ gluon-maven-plugin/src/main/java/io/tomrss/gluon/mavenplugin/CreateProjectMojo.java
@@ -1,9 +1,15 @@
 package io.tomrss.gluon.mavenplugin;
 
+import io.tomrss.gluon.core.GluonBuilder;
+import io.tomrss.gluon.core.persistence.DatabaseVendor;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Optional;
 
 @Mojo(name = "create", requiresProject = false)
 public class CreateProjectMojo extends AbstractMojo {
@@ -20,18 +26,41 @@ public class CreateProjectMojo extends AbstractMojo {
     @Parameter(property = "basePackage")
     private String basePackage;
 
+    @Parameter(property = "templateDirectory")
+    private String templateDirectory;
+
     @Parameter(property = "generationDirectory")
     private String generationDirectory;
 
     @Parameter(property = "rawFilesDirectory")
     private String rawFilesDirectory;
 
+    @Parameter(property = "databaseVendor")
+    private String databaseVendor;
 
-    @Parameter(property = "rawFilesDirectory")
-    private String rawFilesDirectory;
+    @Parameter(property = "templateExtension")
+    private String templateExtension;
 
+    @Parameter(property = "entitiesDirectory")
+    private String entitiesDirectory;
 
     public void execute() throws MojoExecutionException {
-        getLog().info("Hello world");
+        try {
+            final GluonBuilder gluonBuilder = new GluonBuilder();
+
+            Optional.ofNullable(templateDirectory).map(Paths::get).ifPresent(gluonBuilder::templateDirectory);
+            Optional.ofNullable(rawFilesDirectory).map(Paths::get).ifPresent(gluonBuilder::rawFilesDirectory);
+            Optional.ofNullable(generationDirectory).map(Paths::get).ifPresent(gluonBuilder::generationDirectory);
+            Optional.ofNullable(projectGroupId).ifPresent(gluonBuilder::groupId);
+            Optional.ofNullable(projectArtifactId).ifPresent(gluonBuilder::artifactId);
+            Optional.ofNullable(basePackage).ifPresent(gluonBuilder::basePackage);
+            Optional.ofNullable(databaseVendor).map(DatabaseVendor::valueOf).ifPresent(gluonBuilder::databaseVendor);
+            Optional.ofNullable(templateExtension).ifPresent(gluonBuilder::templateExtension);
+            Optional.ofNullable(entitiesDirectory).map(Paths::get).ifPresent(gluonBuilder::readEntitiesFromJson);
+
+            gluonBuilder.createGluon().generateProject();
+        } catch (IOException e) {
+            throw new MojoExecutionException("Unable to generate project", e);
+        }
     }
 }
